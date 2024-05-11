13a60b1f9b622c0414a43d17d8f2cc752d790f34
Hassan Rasool <hassanrasool1057@gmail.com>
2024-05-11 19:11:14 +0500

    Update db.js

--- Frontend/config/db.js
+++ Frontend/config/db.js
@@ -1,7 +1,9 @@
 import mongoose  from "mongoose";
 const connectDB = async()=>{
+    const MONGO_URL='mongodb://localhost:27017';
     try{
-        const conn = await mongoose.connect(process.env.MONGO_URL)
+        const conn = await mongoose.connect(MONGO_URL)
+        // const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log(`Connected to MongoDb Database ${conn.connection.host}`)
     }catch(error)
     {
